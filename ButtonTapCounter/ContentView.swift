d3acd0f3e5107b3466f8d3a75bca79b1bf0982a4
Mark Szymczyk <mark@meandmark.com>
2019-11-20 14:27:42 -0500

    Initial Commit

---
+++ ButtonTapCounter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ButtonTapCounter
+//
+//  Created by mark on 11/20/19.
+//  Copyright © 2019 Swift Dev Journal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
